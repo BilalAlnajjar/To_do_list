42aa6a3dbc448f4ffae20607a983f9f935f9205a
BilalAlnajjar <bilalalnajjar46@gmail.com>
2022-06-01 21:44:38 +0300

    add alert

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css'
+import Toast from "sweetalert2";
 
 class App extends React.Component {
   constructor(props) {
@@ -28,14 +29,41 @@ class App extends React.Component {
   handleSubmit = (event) => {
     event.preventDefault();
     let arr = this.state.tasks;
-    const newTask = {title:this.state.taskValue, checked: false};
-    arr.push(newTask);
-    this.setState({ tasks: arr });
+    const newTask = this.state.taskValue;
 
-    //store new tasks value in the localStorage by convert the array to a JSON string to enable get a transformable value in the next time 
-    //(1) "store array in localStorage": https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage
-    //(2) "JSON.stringify": https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
-    localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
+    if(newTask) {
+      if (arr.length > 0) {
+        if (!arr.includes(newTask)) {
+          console.log("true")
+          arr.push(newTask)
+          this.setState({tasks: arr});
+          Toast.fire({
+            icon: 'success',
+            title: 'Task created successfully'
+          })
+        }else {
+
+          Toast.fire({
+            icon: 'error',
+            title: 'The task is already found'
+          })
+        }
+
+      } else {
+        arr.push(newTask)
+        this.setState({tasks: arr});
+      }
+    }else {
+      Toast.fire({
+        icon: 'error',
+        title: 'The task input is empty'
+      })
+    }
+
+      //store new tasks value in the localStorage by convert the array to a JSON string to enable get a transformable value in the next time
+      //(1) "store array in localStorage": https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage
+      //(2) "JSON.stringify": https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+      localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
   }
 
   deleteItem = (i) => {
@@ -47,7 +75,6 @@ class App extends React.Component {
     //(2) "JSON.stringify": https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
     localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
   }
-
   render() {
     return (
       <section className="container">
@@ -59,7 +86,7 @@ class App extends React.Component {
         <div className="lists" >
           {this.state.tasks.map((element, index) =>
             <div className="listItem" key={index}>
-              <p> {element.title}</p>
+              <p> {element}</p>
               <div>
                 <button className="btnEdit" >edit</button>
                 <button className="btnDelete" onClick={() => this.deleteItem(index)} >delete</button>
